31ed691a662c4e5a61cc51789ca87c3f9e9b9204
supersheep <supersheepxjy@gmail.com>
2014-05-22 14:25:11 +0800

    add every option

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var Event = require("emitter");
 
 function Inspector(opt){
     var doc = this.doc = opt.doc || document;
+    var every = opt.every;
     var self = this;
 
     var dyer = this.dyer = new Dyer({
@@ -25,13 +26,24 @@ function Inspector(opt){
         }
     });
 
-    doc.addEventListener("click", function(e){
+
+    function emit(e){
         if(self._active){
             e.stopPropagation();
             e.preventDefault();
             self.emit("pick",e, e.target);
         }
-    });
+    }
+
+    if(every){
+        var items = doc.querySelectorAll("*");
+        for(var i = 0; i < items.length ; i++){
+            items[i].addEventListener("click",emit);
+        }
+        doc.addEventListener("click", emit);
+    }else{
+        doc.addEventListener("click", emit);
+    }
 }
 
 Event(Inspector.prototype);
